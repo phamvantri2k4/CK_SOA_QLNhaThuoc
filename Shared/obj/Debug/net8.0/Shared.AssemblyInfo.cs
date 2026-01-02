69e7c4ae1f624f49db4b180559a75fb5e290d161
phamvantri2k4 <tripv37@gmail.com>
2026-01-02 12:22:18 +0700

    update ui

--- Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
+++ Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cf2b8ae1c702b1dd0c685db60119e171034614c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d08242f644fb6dfbe14ffb7164fe5bc4eb8ac0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
